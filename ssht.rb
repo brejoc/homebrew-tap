e9d1dcb8fb4ff0249da34cf33b6c9cd1e99d2aed
Jochen Breuer <brejoc@gmail.com>
2015-06-18 21:16:19 +0200

    Update to version 0.4
    Version 0.4 switches to sh instead to bash. There are some Unix
    boxes that don't have bash pre-installed, so sh should be a better
    choice.

--- ssht.rb
+++ ssht.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Ssht < Formula
   homepage 'https://github.com/brejoc/ssht/'
-  url 'https://github.com/brejoc/ssht/archive/0.3.tar.gz'
-  sha1 'bcfde2a7c90fbe88e111cb45c18ebd800e60477f'
+  url 'https://github.com/brejoc/ssht/archive/0.4.tar.gz'
+  sha1 '1ac2c7e44e173baa157c91226374bfc44f61a247'
 
-  head 'https://github.com/brejoc/ssht.git', :revision => '4e895def2b901737ba91bca16b6f708881de18f6'
+  head 'https://github.com/brejoc/ssht.git', :revision => '0fffd24d8c96caac93a19078af15b66de88e84de'
 
   def install
     bin.install "ssht"
