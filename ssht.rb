a302d804262197a0569d1eca70a8b902ead321c4
Jochen Breuer <breuer@dajool.com>
2016-11-22 14:28:24 +0100

    SHA1 ist deprecated
    Added SHA256 hash for ssht.

--- ssht.rb
+++ ssht.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ssht < Formula
   homepage 'https://github.com/brejoc/ssht/'
   url 'https://github.com/brejoc/ssht/archive/0.4.tar.gz'
-  sha1 '1ac2c7e44e173baa157c91226374bfc44f61a247'
+  sha256 '44bc1f6c2e5d80edc87de7a84bfd4bb90cf365219ec954fa1de01ecf4c4aec08'
 
   head 'https://github.com/brejoc/ssht.git', :revision => '0fffd24d8c96caac93a19078af15b66de88e84de'
 
