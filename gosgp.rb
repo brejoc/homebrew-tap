df5eab92fade26378171748723f44f18430cc41a
Jochen Breuer <brejoc@papa.fritz.box>
2022-08-02 17:28:36 +0200

    Fixes build for gosgp
    This also pulls a new version of gosgp, which has an updated
    go.mod and vendored dependencies.

--- gosgp.rb
+++ gosgp.rb
@@ -1,23 +1,18 @@
 require 'formula'
 
-GOPATH = ENV.fetch("HOMEBREW_TEST_TMPDIR") do |k|
-  dir = Dir.mktmpdir("homebrew-tests-", ENV["HOMEBREW_TEMP"] || "/tmp")
-  at_exit { FileUtils.remove_entry(dir) }
-  ENV[k] = dir
-end
-
 class Gosgp < Formula
   homepage 'https://github.com/brejoc/gosgp'
-  url 'https://github.com/brejoc/gosgp/archive/0.1.tar.gz'
-  sha256 '1b55d36b7fc6b7974cb26b8fde808db94d6eb865815ebfc7bc37cd391c1eec29'
+  url 'https://github.com/brejoc/gosgp/archive/0.1.2.tar.gz'
+  sha256 'a864a1a0105a6d721de5e845c8c1ee0524b03936b007b4b7c99e6e57d0237163'
 
-  head 'git@github.com:brejoc/gosgp.git', :revision => '0faceae29ec79b636fe3589601e75e0d577375f8'
+  head 'git@github.com:brejoc/gosgp.git', :revision => '6165666e234ca2f3a56b255261c4f5b40cc0bd37'
 
   depends_on "golang" => :build
 
   def install
-    system "GOPATH=#{GOPATH} go get -d"
-    system "GOPATH=#{GOPATH} go build -o gosgp"
+    ENV["GOPATH"] = buildpath
+    system "go get -d"
+    system "go build -o gosgp"
     bin.install "gosgp"
   end
 end
