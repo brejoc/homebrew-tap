1e416ff5a716d0b16b742bd4b8d503a92489b738
Jochen Breuer <brejoc@gmail.com>
2023-09-26 16:52:25 +0200

    Versioned binary and better version handling in general
      * The binary is now tagged with ldflags during build.
      * Version definition happens in central place.
      * HEAD build is always pointing to main now.

--- stockuploader.rb
+++ stockuploader.rb
@@ -1,5 +1,7 @@
 require 'formula'
 
+VERSION="0.2"
+
 GOPATH = ENV.fetch("HOMEBREW_TEST_TMPDIR") do |k|
   dir = Dir.mktmpdir("homebrew-tests-", ENV["HOMEBREW_TEMP"] || "/tmp")
   at_exit { FileUtils.remove_entry(dir) }
@@ -8,15 +10,22 @@ end
 
 class Stockuploader < Formula
   homepage 'https://github.com/brejoc/stockuploader'
-  url 'https://github.com/brejoc/stockuploader/archive/0.2.tar.gz'
+  url "https://github.com/brejoc/stockuploader/archive/#{VERSION}.tar.gz"
   sha256 '97c54af083f180b55b739b5e7d0cc2fd1ebf1e03897c70d5df70db039556f8d3'
 
-  head 'https://github.com/brejoc/stockuploader.git', :revision => '19177f4db4eb5a9ab92142242040e095e480dbe4'
+  head do
+    url 'https://github.com/brejoc/stockuploader.git', :branch => 'main'
+  end
+
 
   depends_on "golang" => :build
 
   def install
-    system "GOPATH=#{GOPATH} go build"
+    if build.head?
+      VERSION.replace "development"
+    end
+
+    system "GOPATH=#{GOPATH} go build -ldflags=\"-X 'main.Version=#{VERSION}'\""
     bin.install "stockuploader"
   end
 end
