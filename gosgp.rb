a53be80f3542de92f819e68fcb03d4b1e1d5a0f7
Jochen Breuer <brejoc@gmail.com>
2025-07-29 11:16:01 +0200

    Update for gosgp to version 0.1.3

--- gosgp.rb
+++ gosgp.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Gosgp < Formula
   homepage 'https://github.com/brejoc/gosgp'
-  url 'https://github.com/brejoc/gosgp/archive/0.1.2.tar.gz'
-  sha256 'a864a1a0105a6d721de5e845c8c1ee0524b03936b007b4b7c99e6e57d0237163'
+  url 'https://github.com/brejoc/gosgp/archive/0.1.3.tar.gz'
+  sha256 'ec30e48b24ddee10d8c788da04e7445e670633666a2f7c08dd20c9eb67c6a157'
 
-  head 'git@github.com:brejoc/gosgp.git', :revision => '6165666e234ca2f3a56b255261c4f5b40cc0bd37'
+  head 'git@github.com:brejoc/gosgp.git', :revision => '4678698f798974bf70eb798cb34ff22ab62c52c3'
 
   depends_on "golang" => :build
 
